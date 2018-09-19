2693fe597a5471bdc62b6bbce00f218f200b1921
Skyler Burger <skylerburger@gmail.com>
2018-09-18 18:31:25 -0700

    moved quiz to a button and smoothed out some rough edges in the game

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ var gatherUsername = function() {
     var name;
 
     do{
-        name = prompt('Welcome to my site, what\'s your name?');
+        name = prompt('Welcome to my "About Me" quiz game, what\'s your name?');
     } while(!name);
 
     console.log('name: ' + name);
@@ -27,7 +27,7 @@ var question_1 = function() {
     var romCom;
 
     do {
-        var romCom = prompt('Does Skyler enjoy romantic comedy films? *Please answer \'yes\' or \'no\'.'); 
+        var romCom = prompt('Do you think I enjoy romantic comedy films? *Please answer \'yes\' or \'no\'.'); 
     } while (romCom !== 'no' && romCom !== 'yes' && romCom !== 'n' && romCom !== 'y');
 
     romCom = romCom.toLowerCase();
@@ -50,7 +50,7 @@ var question_2 = function() {
     var hellraiser;
 
     do {
-        hellraiser = prompt('Would Skyler be happy to discuss the first 8 out of 10 Hellraiser films with you? *Please answer \'yes\' or \'no\'.');
+        hellraiser = prompt('Have I seen the first 8 out of 10 Hellraiser films with you? *Please answer \'yes\' or \'no\'.');
     } while(hellraiser !== 'no' && hellraiser !== 'yes' && hellraiser !== 'n' && hellraiser !== 'y');
     hellraiser = hellraiser.toLowerCase();
     console.log('Answer 2: ' + hellraiser);
@@ -72,7 +72,7 @@ var question_3 = function() {
     var modPodge;
 
     do {
-        modPodge = prompt('Is Mod Podge Skyler\'s favorite craft supply? *Please answer \'yes\' or \'no\'.');
+        modPodge = prompt('Is Mod Podge my favorite craft supply? *Please answer \'yes\' or \'no\'.');
     } while(modPodge !== 'no' && modPodge !== 'yes' && modPodge !== 'n' && modPodge !== 'y');
     
     modPodge = modPodge.toLowerCase();
@@ -95,17 +95,17 @@ var question_4 = function() {
     var trumpet;
 
     do {
-        trumpet = prompt('Does Skyler know how to play the trumpet? *Please answer \'yes\' or \'no\'.');
+        trumpet = prompt('Do you think I know how to play the trumpet? *Please answer \'yes\' or \'no\'.');
     } while(trumpet !== 'no' && trumpet !== 'yes' && trumpet !== 'n' && trumpet !== 'y');
     
     trumpet = trumpet.toLowerCase();
     console.log('Your answer to the trumpet question is: ' + trumpet);
     
     if(trumpet === 'no' || trumpet === 'n') {
-        alert('Correct, he doesn\'t know how to play the trumpet but he did play the trombone and bassoon for a number of years.');
+        alert('Correct, I don\'t know how to play the trumpet but I did play the trombone and bassoon for a number of years.');
         correctAnswers++;
     } else {
-        alert('Unfortunately playing the trumpet didn\'t seem that appealing. He chose the bassoon instead.');
+        alert('Unfortunately playing the trumpet didn\'t seem that appealing. I chose the bassoon instead.');
     }
     console.log(correctAnswers + ' out of 7 questions correct');
 }
@@ -118,7 +118,7 @@ var question_5 = function() {
     var montana;
 
     do {
-        montana = prompt('Do you think Skyler has ever visited Montana?');
+        montana = prompt('Do you think I have ever visited Montana?');
     } while(montana !== 'no' && montana !== 'yes' && montana !== 'n' && montana !== 'y');
     
     montana = montana.toLowerCase();
@@ -128,7 +128,7 @@ var question_5 = function() {
         alert('Ding ding ding, you\'re correct! It seems like a beautiful place to visit. Maybe someday...');
         correctAnswers++;
     } else {
-        alert('Unfortunately he\'s yet to make it over there. Maybe someday...');
+        alert('Unfortunately I\'ve yet to make it over there. Maybe someday...');
     }
     console.log(correctAnswers + ' out of 7 questions correct');
 }
@@ -140,16 +140,16 @@ var question_5 = function() {
 var question_6 = function() {
     var favNumber = 42;
     var question6Over = false;
+    tries = 4;
     
     
-    var favNumGuess = prompt('Take a stab at guessing Skyler\'s favorite number. Please respond with an integer. You\'ll have 4 chances to guess the right number before I get bored of this.');
+    var favNumGuess = prompt('Take a stab at guessing my favorite number. Please respond with an integer. You\'ll have 4 chances to guess the right number before I get bored of this.');
     favNumGuess = Number(favNumGuess);
     console.log('Guess is ' + favNumGuess);
     
     while(!question6Over) {
         // Checks if guess is correct, alerts if too high or too low
-        tries++;
-        var remainingChances = 4 - tries;
+        tries--;
         console.log('Entering logic flow');
     
         if(favNumGuess === 42) {
@@ -157,14 +157,14 @@ var question_6 = function() {
             correctAnswers++;
             question6Over = true;
         } else if (favNumGuess > 42) {
-            console.log('Remaining chances: ' + remainingChances);
-            alert('Your guess was a little high. You have ' + remainingChances + ' out of 4 chances left.');
+            console.log('Remaining chances: ' + tries);
+            alert('Your guess was a little high. You have ' + tries + ' out of 4 chances left.');
         } else if (favNumGuess < 42) {
-            console.log('Remaining chances: ' + remainingChances);
-            alert('Your guess is a little low. You have ' + remainingChances + ' out of 4 chances left.');
+            console.log('Remaining chances: ' + tries);
+            alert('Your guess is a little low. You have ' + tries + ' out of 4 chances left.');
         } else {
-            console.log('Remaining chances: ' + remainingChances);
-            alert('Did you enter a number? That still counts as a guess unfortunately. You have ' + remainingChances + ' out of 4 chances left.');
+            console.log('Remaining chances: ' + tries);
+            alert('Did you enter a number? That still counts as a guess unfortunately. You have ' + tries + ' out of 4 chances left.');
         }
     
         // If game is still going and all chances are used, too bad
@@ -191,7 +191,7 @@ var question_7 = function() {
     
     
     do {
-        var petNameGuess = prompt('Can you guess the name of one of my previous pets? You have ' + tries + ' remaining');
+        var petNameGuess = prompt('Can you guess the name of one of my previous pets? You have ' + tries + ' chances remaining');
             petNameGuess = petNameGuess.toLowerCase();
             console.log('Guess is ' + petNameGuess);
     
@@ -205,42 +205,46 @@ var question_7 = function() {
         
         if(answerCorrect) {
             console.log(answerCorrect);
-            alert('You got it right');
+            alert('YEP! You got it! Isn\'t that a cute name?');
             correctAnswers++;
             break;
         } else if (petNameGuess) {
             console.log(answerCorrect);
-            alert('Sorry wrong answer');
+            alert('That\'s a pretty cool name but not one I\'ve given to a pet of mine.');
             tries--;
-            alert('You have ' + tries + ' left');
+            alert('You have ' + tries + ' chances remaining. I believe in you!');
             //break;
             console.log(petNameGuess + ' is not ' + petNames[i]);
         }
     
     } while (!answerCorrect && tries > 0);
     
-    alert ('Here is the full list of my past pets\' names: Bubba, Sebastian, KC, Duke, Midnight, Bandit, and Clark.');
+    alert ('Here is the list of my past pets\' names: Bubba, Sebastian, KC, Duke, Midnight, Bandit, and Clark.');
 }
 
 //===============
-// Question Calls
+// Run The Game
 //===============
-var userName = gatherUsername();
-question_1();
-question_2();
-question_3();
-question_4();
-question_5();
-question_6();
-question_7();
 
-//==============
-// Grand Finale
-//==============
-if(correctAnswers === 7) {
-    alert('Congratulations ' + userName + ', you got all of the questions correct! Are you sure you\'re not secretly Skyler?');
-} else if(correctAnswers > 3) {
-    alert('Not bad, ' + userName + '. You got ' + correctAnswers + ' out of 7 correct. I\'d tell you to try better next time but this quiz doesn\'t really matter. You be you!');
-} else {
-    alert(correctAnswers + ' out of 7 isn\'t great, but who really needs to know these things about a stranger, really? Have a great day, ' + userName + '!');
+var beginGame = function() {
+    var userName = gatherUsername();
+    question_1();
+    question_2();
+    question_3();
+    question_4();
+    question_5();
+    question_6();
+    question_7();
+
+    if(correctAnswers === 7) {
+        alert('Congratulations ' + userName + ', you got all of the questions correct! Are you sure you\'re not secretly Skyler?');
+    } else if(correctAnswers > 3) {
+        alert('Not bad, ' + userName + '. You got ' + correctAnswers + ' out of 7 correct. I\'d tell you to try better next time but this quiz doesn\'t really matter. You be you!');
+    } else {
+        alert(correctAnswers + ' out of 7 isn\'t great, but who really needs to know these things about a stranger, really? Have a great day, ' + userName + '!');
+    }
+
+    // Reset the score in case they want to play again
+    // without refreshing the page.
+    correctAnswers = 0;
 }
